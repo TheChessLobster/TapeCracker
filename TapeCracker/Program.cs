35b23ca98ba0a0d360733359ec864f4cd90a1db4
TheChessLobster <60410598+TheChessLobster@users.noreply.github.com>
2020-10-29 02:36:18 -0500

    Grabs all the data.

--- TapeCracker/Program.cs
+++ TapeCracker/Program.cs
@@ -21,13 +21,28 @@ namespace TapeCracker
             var TapeType = args[1];
             var GoalColumns = ColumnGrab(TapeType, args[2]);
             var HeaderRow = CSVSingleLineReader(3, args[0], Convert.ToChar(","));
+            var LoanCount = CSVLineCounter(3, args[0]);
+            List<string[]> loans = new List<string[]>();
             string[] trimmedHeaderRow = new string[62];
             for (int i = 0; i < trimmedHeaderRow.Length; i++){
                     trimmedHeaderRow[i] = HeaderRow[i];
             }
             var LocList = ETLReadyTrimmer.ColumnLocator(GoalColumns, trimmedHeaderRow);
             //We now know which columns of the csv contain the data that we need, so grab this data, and print it off to a new csv
-
+            for(int i = 4; i < LoanCount; i++)
+            {
+                var nonParsed = CSVSingleLineReader(i, args[0], Convert.ToChar(","));
+                var Parsed = new string[LocList.Count()];
+                for(int j = 0;j < nonParsed.Count(); j++)
+                {
+                    for(int k = 0; k < LocList.Count(); k++)
+                    {
+                        if(j == LocList[k]) { Parsed[k] = nonParsed[j]; }
+                    }
+                }
+                loans.Add(Parsed);
+            }
+            var added = loans;
         }
 
         public static string[] ColumnGrab(string DealType, string path)
@@ -47,6 +62,11 @@ namespace TapeCracker
             string[] allLines = File.ReadAllLines(path);
             string[] dealLine = allLines[line].Split(delimiter); // error if less lines, check allLines.Length
             return dealLine;
+        }
+
+        public static int CSVLineCounter(int startLine, string path)
+        {
+            return File.ReadAllLines(path).Count() - startLine;
         }
     }
 }
